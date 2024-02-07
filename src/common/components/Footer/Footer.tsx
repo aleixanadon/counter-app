eba2eb667b5b09d17dcb96443121a683c0f4b3d6
aleix [itb] anadon nunyez <aleix.anadon.7e5@itb.cat>
2024-02-07 17:06:35 +0100

    hotfix: changed wrong aref to correct one

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/aleixanadon/is-your-word-a-palindrome">Source code</a>
+      <a href="https://github.com/aleixanadon/counter-app">Source code</a>
       <a href="https://github.com/aleixanadon">https://github.com/aleixanadon</a>
     </footer>
   )
